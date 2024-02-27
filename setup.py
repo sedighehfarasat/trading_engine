ce168dc75ca027be22bc97e08e49a5ac38f78163
sedigheh <sd.farasat@gmail.com>
2024-02-27 21:07:46 +0330

    Add setup and __init__

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='trading_engine',
+      version='1.0.0',
+      description='A Package for algorithmic trading',
+      url='https://github.com/sedighehfarasat/trading_engine',
+      author='Sedigheh Farasat',
+      author_email='sd.farasat@gmail.com',
+      packages=['trading_engine'],
+      install_requires=[],
+      )
