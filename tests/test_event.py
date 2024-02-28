5499dc534049f7145217ed57555f240746ada7af
sedigheh <sd.farasat@gmail.com>
2024-02-28 16:53:57 +0330

    Test event module

--- tests/test_event.py
+++ tests/test_event.py
@@ -1,3 +1,4 @@
+import datetime
 from trading_engine.event import *
 from unittest import TestCase
 
@@ -10,7 +11,22 @@ class TestEvent(TestCase):
         self.assertEqual(event_type, 'MARKET')
 
     def test_initialize_signal_event(self):
-        event = SignalEvent(123, 'Arian', 123456789, 'BUY')
+        signal_time = datetime.datetime.now().timestamp()
+        event = SignalEvent(123, 'Arian', signal_time, 'BUY')
         event_type = event.type
 
         self.assertEqual(event_type, 'SIGNAL')
+
+    def test_initialize_order_event(self):
+        event = OrderEvent('Arian', 'LMT', 5000, 'BUY')
+        event_type = event.type
+        event.print_order()
+
+        self.assertEqual(event_type, 'ORDER')
+
+    def test_initialize_fill_event(self):
+        fill_time = datetime.datetime.now().timestamp()
+        event = FillEvent(fill_time, 'Arian', 'TSE', 1000, 'BUY', 0, 0)
+        event_type = event.type
+
+        self.assertEqual(event_type, 'FILL')
